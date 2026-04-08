6c056e80f3a2186a1eaf9377e09651e8c035bad7
Nikhil Sasidharan <sasidharann@vmware.com>
2026-04-08 13:40:04 +0800

    meta.go: adding conditions and status

--- pkg/apimachinery/README.md
+++ pkg/apimachinery/README.md
@@ -2,4 +2,9 @@
 
 ### Holds the generic pieces: Metadata, Lists, Watch Events, Status Objects, Conditions, Reusable API Errors as Kubernetes APIs rely heavily on common objet envelopes and resource metadata
 
-#### meta.go will be the first file to be created
+meta.go will be the first file created.
+
+- Contains the metadata for an object (split across TypeMeta and ObjectMeta)
+- Metadata for a List query (invoked by controller/watch/cache)
+- Condition of object
+- Status of API call
--- pkg/apimachinery/meta.go
+++ pkg/apimachinery/meta.go
@@ -29,5 +29,36 @@ type ObjectMeta struct {
 // some objects like PodList do not have ObjectMeta
 // some clients are looking for TypeMeta only (like admissions)
 
+// For Clients:
+// Clients can say "give me a list of Pods after this version". They will get a PodList and a metadata with ResourceVersion
+// or a watch mechanism will start watch based on this ResourceVersion as anchor point
+// or a cache can store the last seen based on this version, and skip reprocessing if nothing changed
+type ListMeta struct {
+	ResourceVersion int64
+}
 
+// What: A strucutured approach for clients to know the current status and why
+// Why: Instead of re-inventing tons of ad-hoc booleans across the code-base, makes easy for tooling to read and display status
+// Type: Ready, Available, Progressing, Failed
+// Status: True, False, Unknown
+// Reason: For code programmatic checks
+// Message: Huma readable details
+// LastTransitionTime: when this condition last changed, for debugging and timeouts
+type Condition struct {
+	Type string
+	Status string
+	Reason string
+	Message string
+	LastTransitionTime time.Time
+}
+
+// What: API operation status, not for k8s objects. Consistent response to tooling
+// Status: Success/Failure
+// Code: HTTP Status code
+type Status struct {
+	Status string
+	Message string
+	Reason string
+	Code int
+}
 
